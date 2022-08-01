d6f6d4b5d422d7ad76b5951bb7ae50b7a8c6064a
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-07-31 21:02:30 -0700

    playing around with adding redux

--- client/src/App.js
+++ client/src/App.js
@@ -8,13 +8,13 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
+import { StoreProvider } from './utils/GlobalState';
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
 
